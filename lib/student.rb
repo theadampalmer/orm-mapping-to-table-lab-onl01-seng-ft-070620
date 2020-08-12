260d046c7b6eb325d16627acc4cb106ed0a6d23a
Adam Palmer <theadampalmer@gmail.com>
2020-08-12 21:38:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,27 @@ attr_accessor :name, :grade
 attr_reader :id
 
 DB[:conn]  
+
+  def initialize
+    @id = nil
+    @name = name
+    @grade = grade
+  end
+
+  def self.create_table
+    
+  end
   
+  def self.drop_table
+    
+  end
+  
+  def save
+    
+  end
+  
+  def self.create
+    
+  end
+    
 end
