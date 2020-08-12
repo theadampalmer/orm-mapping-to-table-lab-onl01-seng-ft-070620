1341d55729cc10f76fe0e2342ce9a6338f79d115
Adam Palmer <theadampalmer@gmail.com>
2020-08-12 21:48:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,7 +20,11 @@ attr_reader :id
   end
   
   def save
-    
+    sql = <<-SQL
+    INSERT INTO students(name, grade) VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() from students")[0][0]
   end
   
   def self.create
